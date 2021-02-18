3516dfac468b62ff17349109cca8545760f61afc
pchtsp <pchtsp@gmail.com>
2021-02-18 14:58:43 +0100

    version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ for r in requirementslist:
 
 kwargs = {
     "name": "orloge",
-    "version": 0.15,
+    "version": 0.17,
     "packages": packages,
     "description": "OR log extractor",
     "long_description": long_description,
