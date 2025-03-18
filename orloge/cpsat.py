d3b102035b4e1d05e0289bcc07b521a1e080f672
Franco Peschiera <pchtsp@gmail.com>
2025-03-18 16:35:33 +0100

    changed to new pypi-published library

--- orloge/cpsat.py
+++ orloge/cpsat.py
@@ -1,6 +1,8 @@
-from cpsat_log_parser.blocks import SearchProgressBlock, SolverBlock, ResponseBlock
+from cpsat_logutils.blocks import SearchProgressBlock, SolverBlock, ResponseBlock
+from cpsat_logutils.blocks.search_progress import ModelEvent
 from .base import LogFile
-import cpsat_log_parser as cpsatlog
+import cpsat_logutils as cpsatlog
+import pandas as pd
 from .constants import (
     LpStatusMemoryLimit,
     LpStatusSolved,
@@ -25,8 +27,50 @@ class CPSAT(LogFile):
         self.my_parser = cpsatlog.LogParser(self.content)
 
     def get_progress(self):
+        """
+        Builds a pandas DataFrame with the search progress.
+        It assumes events are sorted by time.
+        It fills with the previous result when no new information is available.
+        The return DataFrame has the following columns:
+        [the names of columns match the ones in the orloge library,
+        this may not be very relevant to cp-sat]
+        - Time: The time of the event.
+        - CutsBestBound: The best bound found at the time.
+        - BestInteger: The best integer found at the time.
+        - Gap: The gap between the best bound and the best integer.
+        - NumVars: The number of variables before the event.
+        - RemVars: The number of remaining variables after the event.
+        - NumCons: The number of constraints before the event.
+        - RemCons: The number of remaining constraints after the event.
+        """
         progress_block = self.my_parser.get_block_of_type_or_none(SearchProgressBlock)
-        return progress_block.get_table()
+        events = progress_block._parse_events()
+        gap = obj = bound = None
+        cons = new_cons = var = new_var = None
+        my_table = []
+        for e in events:
+            if not isinstance(e, ModelEvent):
+                bound = e.bound
+                obj = e.obj
+                gap = e.get_gap()
+            else:
+                var = e.vars
+                new_var = e.vars_remaining
+                cons = e.constr
+                new_cons = e.constr_remaining
+            my_table.append((e.time, bound, obj, gap, var, new_var, cons, new_cons))
+        col_names = [
+            "Time",
+            "CutsBestBound",
+            "BestInteger",
+            "Gap",
+            "NumVars",
+            "RemVars",
+            "NumCons",
+            "RemCons",
+        ]
+
+        return pd.DataFrame.from_records(my_table, columns=col_names)
 
     def get_first_relax(self, progress):
         return None
